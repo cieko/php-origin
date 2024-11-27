95c47775c9b48b45f7ecb329f5d87ad00531c575
Cieko <110197023+cieko@users.noreply.github.com>
2024-11-28 00:42:46 +0530

    contact page

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $router = new Router();
 
 $router->add('/php-origin/', 'pages/home.php');
 $router->add('/php-origin/about', 'pages/about.php');
+$router->add('/php-origin/contact', 'pages/contact.php');
 
 $router->route($_SERVER['REQUEST_URI']);
 ?>
