ed47ac47a5e47639c4c3b8a03574661358f6d416
Cieko <110197023+cieko@users.noreply.github.com>
2024-11-28 22:07:25 +0530

    slider

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,4 +1,4 @@
-export function setupMenu(burger, overlay) {
+export function setupMenu(burger, overlay, nav) {
   let showMenu = false;
 
   overlay.style.display = "none";
@@ -8,16 +8,26 @@ export function setupMenu(burger, overlay) {
     if (showMenu) {
       burger.classList.add("active");
       overlay.style.display = "block";
+      gsap.to(nav, {
+        backgroundColor: "white",
+        duration: 1,
+        ease: "expo.in",
+      });
       gsap.to(overlay, 1, {
         clipPath: "polygon(0% 0%, 100% 0, 100% 100%, 0% 100%)",
-        ease: "expo.in"
+        ease: "expo.in",
       });
     } else {
       burger.classList.remove("active");
+      gsap.to(nav, {
+        backgroundColor: "transparent",
+        duration: 1,
+        ease: "expo.out",
+      });
       gsap.to(overlay, 1, {
         clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
         ease: "expo.out",
-        onComplete: () => (overlay.style.display = "none")
+        onComplete: () => (overlay.style.display = "none"),
       });
     }
   });
--- views/partials/html-foot.php
+++ views/partials/html-foot.php
@@ -2,6 +2,7 @@
 
 <!-- partial -->
 <script src='https://unpkg.co/gsap@3/dist/gsap.min.js'></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script> -->
 <script src="scripts/main.js" type="module"></script>
 
 </body>
