e4bfd719f8a321fbbfccd62dff239a7905a2e954
Cieko <110197023+cieko@users.noreply.github.com>
2024-11-28 22:11:37 +0530

    changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,169 +1,148 @@
-import { setupMenu } from './menu.js';
-import { setupHeroAnimation } from './heroAnimation.js';
+import { setupMenu } from "./menu.js";
+import { setupHeroAnimation } from "./heroAnimation.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const burger = document.getElementById("burger");
   const overlay = document.querySelector("section");
-  const nav = document.getElementById('navbar');
+  const nav = document.getElementById("navbar");
 
   setupMenu(burger, overlay, nav);
 
   setupHeroAnimation();
 
-  // Slider(all Slides in a container)
-const slider = document.querySelector(".slider")
-// All trails 
-const trail = document.querySelector(".trail").querySelectorAll("div")
+  const slider = document.querySelector(".slider");
 
-// Transform value
-let value = 0
-// trail index number
-let trailValue = 0
-// interval (Duration)
-let interval = 4000
+  const trail = document.querySelector(".trail").querySelectorAll("div");
 
-// Function to slide forward
-const slide = (condition) => {
-    // CLear interval
-    clearInterval(start)
-    // update value and trailValue
-    condition === "increase" ? initiateINC() : initiateDEC()
-    // move slide
-    move(value, trailValue)
-    // Restart Animation
-    animate()
-    // start interal for slides back 
+  let value = 0;
+
+  let trailValue = 0;
+
+  let interval = 4000;
+
+  const slide = (condition) => {
+    clearInterval(start);
+
+    condition === "increase" ? initiateINC() : initiateDEC();
+
+    move(value, trailValue);
+
+    animate();
+
     start = setInterval(() => slide("increase"), interval);
-}
+  };
 
-// function for increase(forward, next) configuration
-const initiateINC = () => {
-    // Remove active from all trails
-    trail.forEach(cur => cur.classList.remove("active"))
-    // increase transform value
-    value === 80 ? value = 0 : value += 20
-    // update trailValue based on value
-    trailUpdate()
-}
+  const initiateINC = () => {
+    trail.forEach((cur) => cur.classList.remove("active"));
 
-// function for decrease(backward, previous) configuration
-const initiateDEC = () => {
-     // Remove active from all trails
-    trail.forEach(cur => cur.classList.remove("active"))
-    // decrease transform value
-    value === 0 ? value = 80 : value -= 20
-     // update trailValue based on value
-    trailUpdate()
-}
+    value === 80 ? (value = 0) : (value += 20);
 
-// function to transform slide 
-const move = (S, T) => {
-    // transform slider
-    slider.style.transform = `translateX(-${S}%)`
-    //add active class to the current trail
-    trail[T].classList.add("active")
-}
+    trailUpdate();
+  };
 
-const tl = gsap.timeline({defaults: {duration: 0.6, ease: "power2.inOut"}})
-tl.from(".bg", {x: "-100%", opacity: 0})
-  .from("p", {opacity: 0}, "-=0.3")
-  .from(".slider-heading", {opacity: 0, y: "30px"}, "-=0.3")
-  .from("button", {opacity: 0, y: "-40px"}, "-=0.8")
+  const initiateDEC = () => {
+    trail.forEach((cur) => cur.classList.remove("active"));
 
-// function to restart animation
-const animate = () => tl.restart()
+    value === 0 ? (value = 80) : (value -= 20);
 
-// function to update trailValue based on slide value
-const trailUpdate = () => {
+    trailUpdate();
+  };
+
+  const move = (S, T) => {
+    slider.style.transform = `translateX(-${S}%)`;
+
+    trail[T].classList.add("active");
+  };
+
+  const tl = gsap.timeline({
+    defaults: { duration: 0.6, ease: "power2.inOut" },
+  });
+  tl.from(".bg", { x: "-100%", opacity: 0 })
+    .from("p", { opacity: 0 }, "-=0.3")
+    .from(".slider-heading", { opacity: 0, y: "30px" }, "-=0.3")
+    .from("button", { opacity: 0, y: "-40px" }, "-=0.8");
+
+  const animate = () => tl.restart();
+
+  const trailUpdate = () => {
     if (value === 0) {
-        trailValue = 0
+      trailValue = 0;
     } else if (value === 20) {
-        trailValue = 1
+      trailValue = 1;
     } else if (value === 40) {
-        trailValue = 2
+      trailValue = 2;
     } else if (value === 60) {
-        trailValue = 3
+      trailValue = 3;
     } else {
-        trailValue = 4
+      trailValue = 4;
     }
-}   
+  };
 
-// Start interval for slides
-let start = setInterval(() => slide("increase"), interval)
+  let start = setInterval(() => slide("increase"), interval);
 
-// Next  and  Previous button function (SVG icon with different classes)
-document.querySelectorAll("svg").forEach(cur => {
-    // Assign function based on the class Name("next" and "prev")
-    cur.addEventListener("click", () => cur.classList.contains("next") ? slide("increase") : slide("decrease"))
-})
+  document.querySelectorAll("svg").forEach((cur) => {
+    cur.addEventListener("click", () =>
+      cur.classList.contains("next") ? slide("increase") : slide("decrease")
+    );
+  });
 
-// function to slide when trail is clicked
-const clickCheck = (e) => {
-    // CLear interval
-    clearInterval(start)
-    // remove active class from all trails
-    trail.forEach(cur => cur.classList.remove("active"))
-    // Get selected trail
-    const check = e.target
-    // add active class
-    check.classList.add("active")
+  const clickCheck = (e) => {
+    clearInterval(start);
 
-    // Update slide value based on the selected trail
-    if(check.classList.contains("box1")) {
-        value = 0
+    trail.forEach((cur) => cur.classList.remove("active"));
+
+    const check = e.target;
+
+    check.classList.add("active");
+
+    if (check.classList.contains("box1")) {
+      value = 0;
     } else if (check.classList.contains("box2")) {
-        value = 20
+      value = 20;
     } else if (check.classList.contains("box3")) {
-        value = 40
+      value = 40;
     } else if (check.classList.contains("box4")) {
-        value = 60
+      value = 60;
     } else {
-        value = 80
+      value = 80;
     }
-    // update trail based on value
-    trailUpdate()
-    // transfrom slide
-    move(value, trailValue)
-    // start animation
-    animate()
-    // start interval
-    start = setInterval(() => slide("increase"), interval)
-}
 
-// Add function to all trails
-trail.forEach(cur => cur.addEventListener("click", (ev) => clickCheck(ev)))
+    trailUpdate();
 
-// Mobile touch Slide Section
-const touchSlide = (() => {
-    let start, move, change, sliderWidth
+    move(value, trailValue);
 
-    // Do this on initial touch on screen
+    animate();
+
+    start = setInterval(() => slide("increase"), interval);
+  };
+
+  trail.forEach((cur) => cur.addEventListener("click", (ev) => clickCheck(ev)));
+
+  const touchSlide = (() => {
+    let start, move, change, sliderWidth;
+
     slider.addEventListener("touchstart", (e) => {
-        // get the touche position of X on the screen
-        start = e.touches[0].clientX
-        // (each slide with) the width of the slider container divided by the number of slides
-        sliderWidth = slider.clientWidth/trail.length
-    })
-    
-    // Do this on touchDrag on screen
+      start = e.touches[0].clientX;
+
+      sliderWidth = slider.clientWidth / trail.length;
+    });
+
     slider.addEventListener("touchmove", (e) => {
-        // prevent default function
-        e.preventDefault()
-        // get the touche position of X on the screen when dragging stops
-        move = e.touches[0].clientX
-        // Subtract initial position from end position and save to change variabla
-        change = start - move
-    })
+      e.preventDefault();
+
+      move = e.touches[0].clientX;
+
+      change = start - move;
+    });
 
     const mobile = (e) => {
-        // if change is greater than a quarter of sliderWidth, next else Do NOTHING
-        change > (sliderWidth/4)  ? slide("increase") : null;
-        // if change * -1 is greater than a quarter of sliderWidth, prev else Do NOTHING
-        (change * -1) > (sliderWidth/4) ? slide("decrease") : null;
-        // reset all variable to 0
-        [start, move, change, sliderWidth] = [0,0,0,0]
-    }
-    // call mobile on touch end
-    slider.addEventListener("touchend", mobile)
-})()
+      change > sliderWidth / 4 ? slide("increase") : null;
+
+      change * -1 > sliderWidth / 4 ? slide("decrease") : null;
+
+      [start, move, change, sliderWidth] = [0, 0, 0, 0];
+    };
+
+    slider.addEventListener("touchend", mobile);
+  })();
 });
